2ead132c8450a6c3a46511ba8024c1fb976bbd75
kkumshayev <kumshkesh@gmail.com>
2017-12-06 16:12:09 +0000

    passing tests for inputting player names and redirecting

--- app.rb
+++ app.rb
@@ -3,8 +3,26 @@ require 'shotgun'
 
 
 class Battle < Sinatra::Base
+  enable :sessions
+
+  get '/first_test' do
+    'Testing infrastructure working!'
+  end
+
   get '/' do
     erb(:player_names)
+  end
+
+  get '/battle' do
+    @p1 = session[:p1]
+    @p2 = session[:p2]
+    "#{@p1} VS #{@p2}"
+  end
+
+  post '/players' do
+    session[:p1] = params[:p1]
+    session[:p2] = params[:p2]
+    redirect '/battle'
   end
 
   run! if app_file == $0
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,14 +1,16 @@
+require 'pry'
+
 feature 'tseting infrastructure working' do
 
   scenario 'homepage content matching' do
-    visit '/'
+    visit '/first_test'
     expect(page).to have_content 'Testing infrastructure working!'
   end
 end
 
 feature 'entering player names' do
 
-  scenario 'player 1' do
+  scenario 'players enter names' do
     visit '/'
     fill_in('p1', with: 'Jimmy')
     fill_in('p2', with: 'Lemonade')

a8def29007a62ebcd6ffda94ac9df9dae74ffd83
kkumshayev <kumshkesh@gmail.com>
2017-12-06 15:00:35 +0000

    failing test for player names input

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -4,5 +4,15 @@ feature 'tseting infrastructure working' do
     visit '/'
     expect(page).to have_content 'Testing infrastructure working!'
   end
+end
 
+feature 'entering player names' do
+
+  scenario 'player 1' do
+    visit '/'
+    fill_in('p1', with: 'Jimmy')
+    fill_in('p2', with: 'Lemonade')
+    click_button("Battle!")
+    expect(page).to have_content("Jimmy VS Lemonade")
+  end
 end

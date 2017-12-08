88ad015cdfbeb323753306f74767287cae29edf9
kkumshayev <kumshkesh@gmail.com>
2017-12-08 11:58:07 +0000

    reverted to working code before sessions

--- app.rb
+++ app.rb
@@ -4,31 +4,27 @@ require './lib/player'
 require './lib/game'
 
 class Battle < Sinatra::Base
-  enable :sessions
-  
+
   get '/' do
     erb(:player_names)
   end
 
   post '/names' do
-    session[:p1] = params[:p1]
-    session[:p2] = params[:p2]
-    p1 = Player.new(session[:p1])
-    p2 = Player.new(session[:p2])
-    $game = Game.new(p1,p2)
-    session[:game] = $game
+    p1 = Player.new(params[:p1])
+    p2 = Player.new(params[:p2])
+    $game = Game.new(p1, p2)
     redirect '/play'
   end
 
   get '/play' do
-    @game = session[:game]
+    @game = $game
     params[:p2hp] = @game.p2.hp
     @p2hp = params[:p2hp]
     erb(:ready)
   end
 
   get '/attack' do
-    @game = session[:game]
+    @game = $game
     @game.attack(@game.p2)
     params[:p2hp] = @game.p2.hp
     @p2hp = params[:p2hp]

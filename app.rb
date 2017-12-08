436c98e5cd719ba10c9cbae1fc03757d44f7e0bd
kkumshayev <kumshkesh@gmail.com>
2017-12-08 12:19:15 +0000

    fixed session_secret and shotgun working

--- app.rb
+++ app.rb
@@ -4,27 +4,34 @@ require './lib/player'
 require './lib/game'
 
 class Battle < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'yes'
 
   get '/' do
     erb(:player_names)
   end
 
   post '/names' do
-    p1 = Player.new(params[:p1])
-    p2 = Player.new(params[:p2])
-    $game = Game.new(p1, p2)
+    session[:p1] = params[:p1]
+    session[:p2] = params[:p2]
+    p1 = Player.new(session[:p1])
+    p2 = Player.new(session[:p2])
+    $game = Game.new(p1,p2)
+    session[:game] = $game
+
     redirect '/play'
   end
 
   get '/play' do
-    @game = $game
+
+    @game = session[:game]
     params[:p2hp] = @game.p2.hp
     @p2hp = params[:p2hp]
     erb(:ready)
   end
 
   get '/attack' do
-    @game = $game
+    @game = session[:game]
     @game.attack(@game.p2)
     params[:p2hp] = @game.p2.hp
     @p2hp = params[:p2hp]

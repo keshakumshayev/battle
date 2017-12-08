fca4086cb02258acbe34f8ddd6b04665e44cc883
kkumshayev <kumshkesh@gmail.com>
2017-12-08 11:56:48 +0000

    sessions implemented in an attempt to make shotgun work

--- app.rb
+++ app.rb
@@ -4,32 +4,35 @@ require './lib/player'
 require './lib/game'
 
 class Battle < Sinatra::Base
-
-
+  enable :sessions
+  
   get '/' do
     erb(:player_names)
   end
 
+  post '/names' do
+    session[:p1] = params[:p1]
+    session[:p2] = params[:p2]
+    p1 = Player.new(session[:p1])
+    p2 = Player.new(session[:p2])
+    $game = Game.new(p1,p2)
+    session[:game] = $game
+    redirect '/play'
+  end
+
   get '/play' do
-    @game = $game
+    @game = session[:game]
     params[:p2hp] = @game.p2.hp
     @p2hp = params[:p2hp]
     erb(:ready)
   end
 
   get '/attack' do
-    @game = $game
+    @game = session[:game]
     @game.attack(@game.p2)
     params[:p2hp] = @game.p2.hp
     @p2hp = params[:p2hp]
     erb(:attack)
-  end
-
-  post '/names' do
-    p1 = Player.new(params[:p1])
-    p2 = Player.new(params[:p2])
-    $game = Game.new(p1, p2)
-    redirect '/play'
   end
 
   run! if app_file == $0
